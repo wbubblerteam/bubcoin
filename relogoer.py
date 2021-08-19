d2cc7059913c88d60e0495ed0963ff6171f84a33
JMcB <joel.mcbride1@live.com>
2021-08-19 18:33:57 +0100

    Autocreate testnet icon as well

--- relogoer.py
+++ relogoer.py
@@ -1,18 +1,30 @@
+import shutil
 from pathlib import Path
+
 import renamer
 
 
 COLOUR_REPLACES = {
-        # Wbubbler dark
+        # Wbubbler dark blue
         '#F9AA4B': '#8ECEEA',
-        # Wbubbler light
+        # Wbubbler light blue
         '#F7931A': '#69BDDE',
     }
+COLOUR_REPLACES_TESTNET = {
+        # Wbubbler medium beige
+        '#F9AA4B': '#F1D4BB',
+        # Wbubbler light beige
+        '#F7931A': '#F4DFCC',
+    }
 SVG_PATH = Path('src/qt/res/src/bitcoin.svg')
+SVG_PATH_TESTNET = SVG_PATH.with_name('bitcoin_testnet.svg')
 
 def main():
-    print('Replacing colours in svg.')
+    print('Copying svg for testnet icon.')
+    shutil.copy(SVG_PATH, SVG_PATH_TESTNET)
+    print("Replacing colours in svg's.")
     renamer.filereplace(SVG_PATH, COLOUR_REPLACES)
+    renamer.filereplace(SVG_PATH_TESTNET, COLOUR_REPLACES_TESTNET)
     print('Requires: inkscape, ImageMagick (convert).')
     print('icns sucks, do it yourself.')
     command = './relogoer.sh'
