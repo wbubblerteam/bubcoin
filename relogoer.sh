6120d1ff134266fc9f1c83080613e4473ce48d32
JMcB <joel.mcbride1@live.com>
2021-09-02 14:06:25 +0100

    Improve relogoer.sh

--- relogoer.sh
+++ relogoer.sh
@@ -9,12 +9,15 @@ inkscape --export-type="png" --export-dpi=72 -w 1024 -h 1024 --export-filename="
 convert "bitcoin.png" -define icon:auto-resize=48,32,16,256 "bitcoin.ico"
 convert "bitcoin_testnet.png" -define icon:auto-resize=16,32,48,256 "bitcoin_testnet.ico"
 cd ../../../../share/pixmaps
-PNG_SRC=../../src/qt/res/icons/bitcoin.png
+png_src=../../src/qt/res/icons/bitcoin.png
 # Likewise, make this other ico
-convert $PNG_SRC -define icon:auto-resize=256,64,48,32,20,16 "bitcoin.ico"
+convert $png_src -define icon:auto-resize=256,64,48,32,20,16 "bitcoin.ico"
 # Generate assorted png's and xpm's
 for size in 16 32 64 128 256
 do
-    convert $PNG_SRC -resize $size "bitcoin$size.png"
-    convert $PNG_SRC -resize $size "bitcoin$size.xpm"
+    convert $png_src -resize $size "bitcoin$size.png"
+
+    size_underscores=$(python3 -c "print('_' * len('$size'))")
+    convert $png_src -resize $size "bitcoin$size_underscores.xpm"
+    mv "bitcoin$size_underscores.xpm" "bitcoin$size.xpm"
 done
