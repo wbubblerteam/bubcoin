0da4a4fd9c7e83691de5f85e09226b97ec802b62
JMcB <joel.mcbride1@live.com>
2021-09-16 15:26:25 +0100

    Revert "Auto replace signet challenge"
    This reverts commit 553513c256aca06f32ad1434c449f67adbd81f2b.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -253,7 +253,7 @@ public:
         vSeeds.clear();
 
         if (!args.IsArgSet("-signetchallenge")) {
-            bin = ParseHex("5121029a7d216d845def2d41967f9235cf63dbe03d2bb67e2fef23f793d37d64406e1a51ae");
+            bin = ParseHex("512103ad5e0edad18cb1f0fc0d28a3d4f1f3e445640337489abb10404f2d1e086be430210359ef5021964fe22d6f8e05b2463c9540ce96883fe3b278760f048f5189f2e6c452ae");
 
 
 
